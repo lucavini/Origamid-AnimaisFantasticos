deefec3acc662dc23d66ba018efd7ff160900cdf
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-07-28 17:35:51 -0300

    modal com javascript

--- js/script.js
+++ js/script.js
@@ -2,8 +2,10 @@ import initScrollSuave from "./modules/scroll-suave.js";
 import initAnimationScroll from "./modules/animacao-scroll.js";
 import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js"
+import iniModal from "./modules/modal.js";
 
 initScrollSuave();
 initAnimationScroll();
 initAccordion();
 initTabNav();
+iniModal();
