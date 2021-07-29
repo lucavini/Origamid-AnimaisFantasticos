da89976b9eb148dfb3aa3333e2c678f76306e07b
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-07-29 18:12:25 -0300

    menu dropDown

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ import initAccordion from "./modules/accordion.js";
 import initTabNav from "./modules/tabnav.js"
 import iniModal from "./modules/modal.js";
 import initTooltip from "./modules/tooltip.js";
+import initDropDownMenu from "./modules/dropDownMenu.js";
 
 initScrollSuave();
 initAnimationScroll();
