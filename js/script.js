d391edd7d27e2a671dd83c6a4134c35dbebfff69
Lucas Vinicius <lucass.lv.lv@gmail.com>
2021-08-18 10:44:03 -0300

    funcionamento refatorado

--- js/script.js
+++ js/script.js
@@ -5,7 +5,7 @@ import Modal from './modules/modal.js';
 import Tooltip from './modules/tooltip.js';
 import DropDownMenu from './modules/dropDownMenu.js';
 import MenuMobile from './modules/menuMobile.js';
-import initFuncionamento from './modules/funcionamento.js';
+import Funcionamento from './modules/funcionamento.js';
 import fetchAnimais from './modules/fetchAnimais.js';
 import fetchBitcoin from './modules/fetchBitcoin.js';
 import ScrollAnima from './modules/scroll-anima.js';
@@ -45,6 +45,8 @@ const menuMobile = new MenuMobile(
 );
 menuMobile.init();
 
-initFuncionamento();
+const funcionamento = new Funcionamento('[data-semana]', 'aberto');
+funcionamento.init();
+
 fetchAnimais('../animaisApi.json', '.numeros-grid');
 fetchBitcoin('https://blockchain.info/ticker', '.btc-preco');
